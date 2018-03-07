8bf04af0ade4a0e7017bea6d754c10d0a1aaa124
EstevesAndre <estevesandre1@hotmail.com>
2018-03-07 20:28:09 +0000

    Added couch, still in progress

---
+++ WebCGF.tp1/tp1/myCouch.js
@@ -0,0 +1,45 @@
+/**
+ * myCouch
+ * @param gl {WebGLRenderingContext}
+ * @constructor
+ */
+
+
+class myCouch extends CGFobject
+{
+  constructor(scene) 
+	{
+		super(scene);
+		this.sofa=new MyUnitCubeQuad(this.scene);
+	};  
+
+	display()
+	{	
+		//sides
+		this.scene.pushMatrix();
+		this.scene.translate(3,1,0.25);
+		this.scene.scale(1,2,3.5);
+		this.sofa.display();
+		this.scene.translate(-6,0,0);
+		this.sofa.display();
+		this.scene.popMatrix();
+	
+		// back
+		this.scene.pushMatrix();
+		this.scene.translate(0,1.5,-1);
+		this.scene.scale(5,3,1);
+		this.sofa.display();
+		this.scene.popMatrix();
+		
+		// bottom
+		this.scene.pushMatrix();
+		this.scene.translate(0,0.5,0.75);
+		this.scene.scale(5,1,2.5);
+		this.sofa.display();
+		this.scene.popMatrix();
+
+
+	};
+};
+
+
