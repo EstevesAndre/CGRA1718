a782f8fd4c7cf4d387b3c2fd197c7def52135f01
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2018-03-12 16:17:35 +0000

    Added tp3 files

---
+++ WebCGF.tp3/tp3/MyTable.js
@@ -0,0 +1,47 @@
+/**
+ * myTable
+ * @param gl {WebGLRenderingContext}
+ * @constructor
+ */
+
+class myTable extends CGFobject
+{
+	constructor(scene) 
+	{
+		super(scene);
+		this.cube = new MyUnitCubeQuad(this.scene);
+	};
+
+	display()
+	{
+		this.scene.pushMatrix();
+		this.scene.translate(0, 3.5 + 0.3/2, 0);
+		this.scene.scale(5, 0.3, 3);
+		this.cube.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+		this.scene.translate(5/2.0 - 0.3/2, 3.5/2, 3/2.0 - 0.3/2);
+		this.scene.scale(0.3, 3.5, 0.3);
+		this.cube.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+		this.scene.translate(-5/2.0 + 0.3/2, 3.5/2, 3/2.0 - 0.3/2);
+		this.scene.scale(0.3, 3.5, 0.3);
+		this.cube.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+		this.scene.translate(5/2.0 - 0.3/2, 3.5/2, -3/2.0 + 0.3/2);
+		this.scene.scale(0.3, 3.5, 0.3);
+		this.cube.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+		this.scene.translate(-5/2.0 + 0.3/2, 3.5/2, -3/2.0 + 0.3/2);
+		this.scene.scale(0.3, 3.5, 0.3);
+		this.cube.display();
+		this.scene.popMatrix();
+	}
+};
