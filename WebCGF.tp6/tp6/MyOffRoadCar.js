bae1e0d7a039718a7bee0ab1bd632e117cc3b80b
estevesandre <up201606673@fe.up.pt>
2018-04-29 02:53:14 +0100

    created 2 classes, sphericalBow not completed

---
+++ WebCGF.tp6/tp6/MyOffRoadCar.js
@@ -0,0 +1,67 @@
+/**
+ * MyOffRoadCar
+ * @param gl {WebGLRenderingContext}
+ * @constructor
+ */
+
+class MyOffRoadCar extends CGFobject
+{
+
+	constructor(scene)
+	{
+		super(scene);
+
+		this.wheel = new MyWheel(this.scene,12,1);
+		
+		this.chassi = new MyCarChassi(this.scene,0);	
+
+		this.chassiAppearance = new CGFappearance(this.scene);
+		this.chassiAppearance.loadTexture("../resources/images/chassi.jpg");
+	};
+
+	display() 
+	{				
+		this.scene.pushMatrix();
+			this.scene.translate(-5,1.8,-4);
+			this.scene.scale(1.7,1.7,1.7);
+			this.scene.rotate(this.wheel.angle,0,0,1);	
+			this.wheel.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();			
+			this.scene.translate(4,1.8,-4);			
+			this.scene.scale(1.7,1.7,1.7);
+			this.scene.rotate(this.wheel.angle,0,0,1);	
+			this.wheel.display();
+		this.scene.popMatrix();	
+			
+		this.scene.pushMatrix();	
+			this.scene.translate(4,1.8,4.1);
+			this.scene.scale(1.7,1.7,1.7);
+			this.scene.rotate(Math.PI,1,0,0);			
+			this.scene.rotate(-this.wheel.angle,0,0,1);	
+			this.wheel.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();			
+			this.scene.translate(-5,1.8,4.1);
+			this.scene.scale(1.7,1.7,1.7);				
+			this.scene.rotate(Math.PI,1,0,0);	
+			this.scene.rotate(-this.wheel.angle,0,0,1);	
+			this.wheel.display();
+		this.scene.popMatrix();
+
+		this.chassiAppearance.apply();
+		this.scene.pushMatrix();
+			this.scene.translate(0,2,0);
+			this.chassi.display();
+			this.scene.scale(1,1,-1);
+			this.chassi.display();
+		this.scene.popMatrix();
+	};
+
+	update(currTime)
+	{
+		this.wheel.update(currTime);
+	}
+};
